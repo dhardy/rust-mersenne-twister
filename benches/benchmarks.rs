2545958e7c9dec6923e6c42291057ab27e876948
Diggory Hardy <git@dhardy.name>
2017-07-25 10:49:12 +0100

    Add benchmarks generating a full cycle of values

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -22,7 +22,35 @@ mod mt19937 {
             // Note that the first call to next_u32() triggers a call
             // to the fill_next_state() method, which is really what I
             // want to benchmark here.
-            mt.next_u32();
+            mt.next_u32()
+        })
+    }
+    
+    #[bench]
+    fn benchmark_gen_624_u32(b: &mut ::test::Bencher) {
+        let mut mt = MT19937::new_unseeded();
+        // Note that next_u32() periodically calls fill_next_state() every 624
+        // uses, hence generate this many to get an average.
+        b.iter(|| {
+            let mut r = 0;
+            for _ in 0..624 {
+                r = mt.next_u32();
+            }
+            r   // return last value to prevent over-optimisation
+        })
+    }
+    
+    #[bench]
+    fn benchmark_gen_312_u64(b: &mut ::test::Bencher) {
+        let mut mt = MT19937::new_unseeded();
+        // Note that next_u64() periodically calls fill_next_state() every 312
+        // uses, hence generate this many to get an average.
+        b.iter(|| {
+            let mut r = 0;
+            for _ in 0..312 {
+                r = mt.next_u64();
+            }
+            r   // return last value to prevent over-optimisation
         })
     }
 }
@@ -45,7 +73,35 @@ mod mt19937_64 {
             // Note that the first call to next_u32() triggers a call
             // to the fill_next_state() method, which is really what I
             // want to benchmark here.
-            mt.next_u64();
+            mt.next_u64()
+        })
+    }
+    
+    #[bench]
+    fn benchmark_gen_624_u32(b: &mut ::test::Bencher) {
+        let mut mt = MT19937_64::new_unseeded();
+        // Note that next_u32() periodically calls fill_next_state() every 312
+        // uses. Use 624 to be equivalent to 32-bit implementation.
+        b.iter(|| {
+            let mut r = 0;
+            for _ in 0..624 {
+                r = mt.next_u32();
+            }
+            r   // return last value to prevent over-optimisation
+        })
+    }
+    
+    #[bench]
+    fn benchmark_gen_312_u64(b: &mut ::test::Bencher) {
+        let mut mt = MT19937_64::new_unseeded();
+        // Note that next_u64() periodically calls fill_next_state() every 312
+        // uses, hence generate this many to get an average.
+        b.iter(|| {
+            let mut r = 0;
+            for _ in 0..312 {
+                r = mt.next_u64();
+            }
+            r   // return last value to prevent over-optimisation
         })
     }
 }
